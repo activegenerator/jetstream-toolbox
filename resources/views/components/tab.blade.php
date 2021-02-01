27f24262956fb7de5e51437d4c03ed9b5dd58628
Lennard <lennard@spring.nl>
2021-02-01 13:26:47 +0100

    Tabs

---
+++ resources/views/components/tab.blade.php
@@ -0,0 +1,18 @@
+@props(['name'])
+
+<div x-data="{
+        id: '',
+        name: '{{ $name }}',
+        show: false,
+        showIfActive(active) {
+            this.show = (this.name === active);
+        }
+    }"
+     wire:ignore.self wire:key="{{ $name }}"
+     x-show="show"
+     role="tabpanel"
+     :aria-labelledby="`tab-${id}`"
+     :id="`tab-panel-${id}`"
+>
+    {{ $slot }}
+</div>
---
+++ resources/views/components/tabs.blade.php
@@ -0,0 +1,47 @@
+@props([
+    'active',
+    'class' => 'border-blue-500'
+])
+
+<div x-data="{
+        activeTab: '{{ $active }}',
+        tabs: [],
+        tabHeadings: [],
+        toggleTabs() {
+            this.tabs.forEach(
+                tab => tab.__x.$data.showIfActive(this.activeTab)
+            );
+        }
+     }"
+     x-init="() => {
+        tabs = [...$refs.tabs.children];
+        tabHeadings = tabs.map((tab, index) => {
+            tab.__x.$data.id = (index + 1);
+            return tab.__x.$data.name;
+        });
+        toggleTabs();
+     }"
+>
+    <div class="mb-3 border-b"
+         role="tablist"
+    >
+        <template x-for="(tab, index) in tabHeadings"
+                  :key="index"
+        >
+            <button x-text="tab"
+                    type="button"
+                    @click="console.log(tab);activeTab = tab; toggleTabs();"
+                    class="-mb-px px-4 rounded-none py-2 text-sm focus:border-none focus:outline-none"
+                    :class="tab === activeTab ? 'border-b-2 {{ $class }}' : 'text-gray-800'"
+                    :id="`tab-${index + 1}`"
+                    role="tab"
+                    :aria-selected="(tab === activeTab).toString()"
+                    :aria-controls="`tab-panel-${index + 1}`"
+            ></button>
+        </template>
+    </div>
+
+    <div x-ref="tabs">
+        {{ $slot }}
+    </div>
+</div>
